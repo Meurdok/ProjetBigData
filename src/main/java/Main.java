52749f62a973adbb9ebcc0d5aac0271180c6750d
Meurdok <decanterp@hotmail.com>
2019-01-11 15:42:58 +0100

    Partie 1 finie :
    - Comptage du nombre de connexion par user
    - Affichage sur la sortie standard du top 1O des utilisation
    - Stockage du RDD dans un fichier

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.apache.spark.SparkConf;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -26,9 +27,32 @@ public class Main {
         System.out.println("Total words: " + counts.count());
         counts.saveAsTextFile("out/authCount");
         */
+
+        //Recuperer col 2 et 4
+
+        //PART 1
+
+        //Nettoyage des ligne contenant un "?"
         JavaRDD<String> clean = textFile.filter(line -> !line.contains("?"));
-        clean.saveAsTextFile("out/clean");
+
+        //Comptage du nombre d'utilisation des machine par chaque utilisateur
+        JavaPairRDD<String, Integer> taux = clean.map(s -> Arrays.asList(s.split(",")))
+                                    .mapToPair(w -> new Tuple2<>("["+ w.get(1)+"-"+w.get(3)+"]",1))
+                                    .reduceByKey((a,b) -> a + b);
+
+
+        //Inversion de la RDD
+
+        JavaPairRDD<Integer,String> reversed = taux.mapToPair(s -> s.swap());
+
+        //Affichage sur la sortie standard des 10 accès les plus fréquents
+        List<Tuple2<Integer, String>> top10 = reversed.sortByKey(false).take(10);
+        top10.forEach(s -> System.out.println(s));
+
+        //Output dans le dossier out/clean
+        taux.saveAsTextFile("out/clean");
         System.out.println("DONE");
     }
 
 }
+
